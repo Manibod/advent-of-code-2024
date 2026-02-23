b38bdcf1b3498bae44a4eb544115ea3e5f0265ec
Manibod <virasonemanibod@hotmail.com>
2026-02-22 22:30:35 -0500

    day 18 part 1

---
+++ 18/main.py
@@ -0,0 +1,111 @@
+"""
+Script to solve an advent of code problem.
+"""
+
+# Standard library imports
+import heapq
+import os
+import logging
+import sys
+
+def main():
+   script_directory = os.path.dirname(os.path.abspath(__file__))
+
+   # _solve_problem(f"{script_directory}/data_small.txt")
+   # Uncomment following line when the problem can be solved with the small data.
+   _solve_problem(f"{script_directory}/data.txt")
+
+
+def _solve_problem(data_file):
+   "Function to solve the corresponding advent of code problem using the data in 'data_file'."
+   # Ensure that data file exists
+   if not os.path.isfile(data_file):
+      logging.error("Data file does not exist.")
+      sys.exit(1)
+
+   # Ensure that input exists
+   if os.path.getsize(data_file) <= 0:
+      logging.error("Data file is still empty.")
+      sys.exit(1)
+
+
+   NUM_BYTE_FALLEN = 1024
+   SIZE = 70
+   POS_START = (0, 0)
+   POS_END = (SIZE, SIZE)
+
+   class PosPath:
+      def __init__(self, x, y, g):
+         self.x = x
+         self.y = y
+         self.g = g
+         self.h = abs(SIZE - x) + abs(SIZE - y)
+
+      def __lt__(self, other):
+         return (self.g + self.h) < (other.g + other.h)
+   
+   class BestCost:
+      def __init__(self, g, x_p, y_p):
+         self.g   = g
+         self.x_p = x_p
+         self.y_p = y_p
+
+   byte_coord_list = []
+   with open(data_file, 'r') as f:
+      for line in f:
+         line_split = line.split(',')
+         byte_coord_list.append((int(line_split[1]), int(line_split[0])))
+   byte_coord_set = set(byte_coord_list[:NUM_BYTE_FALLEN])
+
+   pos_start = PosPath(0, 0, 0)
+   
+   grid_pos_best_cost = {}
+   grid_pos_best_cost[pos_start] = BestCost(0, 0, 0)
+
+   heap =[]
+   heapq.heappush(heap, pos_start)
+
+   goal_found = False
+
+   while heap and not goal_found:
+      pos = heapq.heappop(heap)
+      pos_neigh = [PosPath(pos.x - 1, pos.y,     pos.g + 1),
+                   PosPath(pos.x + 1, pos.y,     pos.g + 1),
+                   PosPath(pos.x,     pos.y - 1, pos.g + 1),
+                   PosPath(pos.x,     pos.y + 1, pos.g + 1)]
+      for pos_n in pos_neigh:
+         if not (0 <= pos_n.x and pos_n.x <= SIZE and 0 <= pos_n.y and pos_n.y <= SIZE) or \
+            (pos_n.x, pos_n.y) in byte_coord_set:
+            continue
+
+         if ((pos_n.x, pos_n.y) not in grid_pos_best_cost or
+             pos_n.g < grid_pos_best_cost[(pos_n.x, pos_n.y)].g):
+            grid_pos_best_cost[(pos_n.x, pos_n.y)] = BestCost(pos_n.g, pos.x, pos.y)
+            heapq.heappush(heap, PosPath(pos_n.x, pos_n.y, pos_n.g))
+
+            if (pos_n.x, pos_n.y) == POS_END:
+               goal_found = True
+               break
+
+   pos_best_path = set()
+   pos = POS_END
+   while pos != POS_START:
+      pos_best_path.add(pos)
+      pos = (grid_pos_best_cost[pos].x_p, grid_pos_best_cost[pos].y_p)
+
+   grid_solution = ['.' * (SIZE + 1)] * (SIZE + 1)
+
+   for bc in byte_coord_set:
+      grid_solution[bc[0]] = grid_solution[bc[0]][:bc[1]] + '#' + grid_solution[bc[0]][bc[1] + 1:]
+
+   for p in pos_best_path:
+      grid_solution[p[0]] = grid_solution[p[0]][:p[1]] + 'O' + grid_solution[p[0]][p[1] + 1:]
+   
+   print(f'STEP: {len(pos_best_path)}')
+
+   with open('./18/grid_solution.txt', 'w') as f:
+      for row in grid_solution:
+         f.write(row + '\n')
+
+if __name__ == "__main__":
+   main()
